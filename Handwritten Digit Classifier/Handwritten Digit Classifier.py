a929e5c09fe3f70929256d68870a8ede09ab8609
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-09-02 22:28:51 +0600

    Handwritten Digit Classifier.py is modified

--- Handwritten Digit Classifier/Handwritten Digit Classifier.py
+++ Handwritten Digit Classifier/Handwritten Digit Classifier.py
@@ -16,6 +16,18 @@ def draw_test_image(name, pred, input_img):
                 cv2.FONT_HERSHEY_COMPLEX_SMALL, 4, (255, 0, 0), 2)
     cv2.imshow(name, expanded_image)
     
+for i in range(0, 10):
+    rand = np.random.randint(0,len(x_test))
+    input_img = x_test[rand]
+
+    imageL = cv2.resize(input_img, None, fx=4, fy=4, interpolation = cv2.INTER_CUBIC) 
+    input_img = input_img.reshape(1,28,28,1)
+    
+    res = str(classifier.predict_classes(input_img, 1, verbose = 0)[0])
+    draw_test_image("Prediction", res, imageL) 
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
 
 
 
